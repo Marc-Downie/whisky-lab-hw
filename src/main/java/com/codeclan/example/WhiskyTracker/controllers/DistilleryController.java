73fb0925010e7e282375c3946f29d29d67045783
Ruth Foulis <ruthlilfoulis@gmail.com>
2021-01-20 16:21:35 +0000

    some routes working

--- src/main/java/com/codeclan/example/WhiskyTracker/controllers/DistilleryController.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/controllers/DistilleryController.java
@@ -1,10 +1,13 @@
 package com.codeclan.example.WhiskyTracker.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DistilleryController {
+
+//    @Autowired
 
 
 }
